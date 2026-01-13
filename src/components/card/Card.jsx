614648c0fe184052ed855bb0752f646d94b172fa
lord <danipramoedya2002@gmail.com>
2026-01-13 15:34:40 +0700

    feat(card): update package card components to make them better

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,37 +8,37 @@ export default function Card({
   pic, title, type, description, price
 }) {
   return (
-    <div className="shadow bg-white rounded-2xl overflow-hidden w-full max-w-sm mx-auto flex flex-col h-full">
+    <div className="shadow-md bg-white py-6 rounded-2xl overflow-hidden w-full max-w-sm mx-auto flex flex-col h-full">
       {/* Gambar - tetap ratio yang bagus */}
-      <div className="px-4 pt-4">
+      <div className="px-5">
         <img
           src={pic}
           alt="Package"
-          className="w-full h-48 sm:h-56 object-cover rounded-xl"
+          className="w-full h-62 object-cover rounded-xl"
         />
       </div>
 
       {/* Bronze + Best Seller */}
-      <div className="px-4 pt-4 flex justify-between items-center">
-        <h3 className="text-xl sm:text-2xl font-bold text-orange-400">{title}</h3>
-        <div className="px-6 py-2 bg-teal-100 text-teal-600 rounded-l-full text-sm font-medium">
+      <div className="ps-5 mt-4 flex justify-between items-center">
+        <h3 className="text-md sm:text-lg font-bold text-orange-400">{title}</h3>
+        <div className="px-6 py-2 bg-teal-100 text-teal-600 rounded-l-full text-lg sm:text-xl font-bold">
           {type}
         </div>
       </div>
 
       {/* Detail paket */}
-      <div className="px-4 pt-4 pb-8 flex flex-col flex-grow">
-        <h2 className="text-lg sm:text-xl font-bold text-slate-800 leading-tight">
+      <div className="px-5 mt-4 flex flex-col flex-grow">
+        <h2 className="text-lg sm:text-xl font-bold text-slate-800">
           {description}
         </h2>
 
-        <p className="mt-2 text-2xl font-bold text-slate-800">
+        <p className="mt-4 text-2xl font-bold text-slate-800">
           {price}
-          <span className="text-sm sm:text-base text-teal-600 font-bold"> person</span>
+          <span className="text-teal-600"> person</span>
         </p>
 
         {/* Icons */}
-        <div className="flex gap-3 mt-6">
+        <div className="mt-4 flex gap-3">
           <div className="p-3 bg-teal-100 rounded-xl">
             <FaPlaneDeparture className="w-6 h-6 text-teal-600" />
           </div>
@@ -56,7 +56,7 @@ export default function Card({
         {/* Button - tetap di bawah */}
         <button
           className="
-            mt-8 w-full py-3.5 rounded-lg text-sm font-semibold
+            mt-4 w-full py-3.5 rounded-lg text-sm font-semibold
             border-2 border-orange-300 text-orange-300
             hover:bg-orange-300 hover:text-white
             transition-all duration-300
